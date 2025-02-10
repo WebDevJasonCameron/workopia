7d200014085b49e3cf730b17d746cd93268ac228
Jason Cameron <dev.jasoncameron@gmail.com>
2025-02-10 05:45:55 -0600

    mod bootstrap and study routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return '<h1> hello </h1>';
+});
+
+Rout::post('/submit', function () {
+    return 'Submitted';
+});
