2d2a2cdb704b11eecefe13f241d01dd116d19585
Jason Cameron <dev.jasoncameron@gmail.com>
2025-03-11 20:24:32 -0500

    remove bm

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -25,7 +25,7 @@ class BookmarkController extends Controller
 
     /** Create new bookmark
      * 
-     * @route POST "/bookmarks"
+     * @route POST "/bookmarks/{job}"
      * @param Job $job
      * @return RedirectResponse
      */
@@ -35,12 +35,33 @@ class BookmarkController extends Controller
 
         // Check if the job is already bookmarked
         if ($user->bookmarkedJobs()->where('job_id', $job->id)->exists()) {
-            return back()->with('status', 'Job is already bookmarked');
+            return back()->with('error', 'Job is already bookmarked');
         }
 
         // Create new Bookmared 
         $user->bookmarkedJobs()->attach($job->id);
 
         return back()->with('success', 'Job bookmarked successfully!');
+    }
+
+    /** Remove bookmark job
+     * 
+     * @route DELETE "/bookmarks/{job}"
+     * @param Job $job
+     * @return RedirectResponse
+     */
+    public function destroy(Job $job): RedirectResponse
+    {
+        $user = Auth::user();
+
+        // Check if the job is not bookmarked
+        if (!$user->bookmarkedJobs()->where('job_id', $job->id)->exists()) {
+            return back()->with('error', 'Job is not bookmarked');
+        }
+
+        // Remove bookmared 
+        $user->bookmarkedJobs()->detach($job->id);
+
+        return back()->with('success', 'Bookmarked removed successfully!');
     }
 }
